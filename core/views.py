af2ce6346d946ace59e3be4cbe485ab54f4373bc
DJ Knox <dhknox@gmail.com>
2019-08-23 14:04:43 -0400

    install django-rest-framework and add generic Post views

--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from rest_framework import generics
+from core.models import Post
+from core.serializers import PostSerializer
 
 
-def index(request):
-    return HttpResponse('Hello, World!')
+class PostList(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
