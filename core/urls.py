e38b5a6e9f0bec3f9fcd84e094e59b34ae76555e
DJ Knox <dhknox@gmail.com>
2019-08-23 15:05:34 -0400

    add django-rest-auth for Token Authentication

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('', views.PostViewSet, base_name='posts')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('rest-auth/', include('rest_auth.urls')),
+]
